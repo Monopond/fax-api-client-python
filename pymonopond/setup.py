d672b91cf5d9eba1ead28a5da24a96fe778f6223
JC Francisco <jc.francisco@orangeandbronze.com>
2013-08-23 00:49:25 +0800

    committing .gitignore and setup.py
    committing .gitignore and setup.py
    
    Signed-off-by: JC Francisco <jc.francisco@orangeandbronze.com>

--- pymonopond/setup.py
+++ pymonopond/setup.py
@@ -4,13 +4,15 @@ except ImportError:
 	from distutils.core import setup
 
 config = {
-	'description': 'Python Client for Monpond',
-	'author': ['JC Francisco', 'Froilan Co'],
+	'name': 'pymonopond',
+	'description': 'Python Client for Monopond',
+	'author': 'Froilan Co',
 	'url': 'URL to get it at.',
-	'download_url': 'Where to download it.',
-	'author_email': 'My email.',
+	'download_url': 'https://github.com/Monopond/fax-api-client-python',
+	'author_email': 'froilan.co@orangeandbronze.com',
 	'version': '2.0',
 	'install_requires': ['suds'],
+    'tests_require': ['mockito'],
 	'packages': ['pymonopond'],
 }
 
